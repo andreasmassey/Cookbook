468b54c8b4cce0a7b7437371ab2e61055951c8af
andreasmassey <andreas@masseyfam.com>
2022-05-27 20:17:45 -0400

    Initial Push

---
+++ Cookbook/Data/CookbookContext.cs
@@ -0,0 +1,45 @@
+﻿using Cookbook.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Cookbook.Data
+{
+    public class CookbookContext : DbContext
+    {
+        private readonly IConfiguration _config;
+
+        public CookbookContext(IConfiguration config, DbContextOptions<CookbookContext> options)
+            : base(options)
+        {
+            _config = config;
+        }
+
+        public DbSet<RecipeEntity> Recipes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _config["ConnectionStrings:CookbookDbConnection"];
+            optionsBuilder.UseSqlServer(connectionString, sqlServerOptions => sqlServerOptions.CommandTimeout(120));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<RecipeEntity>(entity =>
+            {
+                entity.ToTable("Recipe");
+                entity.HasKey(e => e.RecipeID);
+
+                entity.Property(e => e.Title)
+                    .HasColumnType("nvarchar(256)");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.CreatedBy)
+                    .HasColumnType("nvarchar(50)");
+            });
+        }
+    }
+}
---
+++ Cookbook/Models/Entities/RecipeEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookbook.Models.Entities
+{
+    public class RecipeEntity
+    {
+        public long RecipeID { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
